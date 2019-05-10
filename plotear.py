baca7c35927f4991991455a28211cd475540d4f2
gabriel <gabriel.hruza@gmail.com>
2019-05-10 10:48:50 -0300

    update plotear.py - plot_linea recursivo

--- plotear.py
+++ plotear.py
@@ -2,6 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+def plot_linea(n, min_y, max_y, min_x, max_x):
+    if n.hoja:
+        print("hoja")
+        return
+    else:
+        if n.label == "x":
+            plt.plot([n.umbral, n.umbral], [min_y, max_y])
+
+            #max_y = n.umbral
+            plot_linea(n.lc, min_y, max_y, min_x, max_x)  # voy por la izquierda
+            #min_y = n.umbral
+            plot_linea(n.rc, min_y, max_y, min_x, max_x)  # voy por la derecha
+
+        else:
+            plt.plot([min_x, max_x], [n.umbral, n.umbral])
+
+            #min_x=n.umbral
+            plot_linea(n.lc, min_y, max_y, min_x, max_x)  # voy por la izquierda
+            #max_x = n.umbral
+            plot_linea(n.rc, min_y, max_y, min_x, max_x)  # voy por la derecha
+
 def plotear(dataset, arbol, titulo):
 
     path = 'res/tree.svg'
@@ -9,29 +30,20 @@ def plotear(dataset, arbol, titulo):
     colors = np.where(dataset['clase'] == 1, 'r', 'k') #clase 1 = rojo // clase0 = negro
     dataset.plot(kind="scatter", x="x", y="y", s=20, c=colors)
 
-    techo_x = dataset["x"].max()
-    piso_x  = 0
-    techo_y = dataset["y"].max()
-    piso_y  = 0
-    resg_label = "x"
+    max_x = dataset["x"].max()
+    min_x  = 0
+    max_y = dataset["y"].max()
+    min_y  = 0
 
-    #for nodo in arbol.arbol:
-
-        #if nodo.label == "x":
-         #   plt.plot([nodo.umbral,nodo.umbral],[piso_y,techo_y])
-
-        #else:
-            #if nodo.label != resg_label:
-                # actualizar techo
-             #   techo_x = nodo.parent.umbral
-
-         #   plt.plot([piso_x,techo_x], [nodo.umbral,nodo.umbral])
+    n = arbol.raiz()
+    plot_linea(n, min_y, max_y, min_x, max_x)
 
     plt.title(titulo)
     plt.savefig(path)
 
     if arbol is not None:
         arbol.asignar_id()
+        arbol.imprimir()
         arbol.export("mi arbol", "arbol.gv")
 
 
